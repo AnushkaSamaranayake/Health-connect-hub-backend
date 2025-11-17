374c27703777988b177faba5fe3f3d94904372da
AnushkaSamaranayake <anushkasandeepa522@gmail.com>
2025-11-17 20:18:53 +0530

    added maven project

---
+++ src/main/java/com/hch/hch_backend/HchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hch.hch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HchBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hch/hch_backend/HchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hch.hch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
